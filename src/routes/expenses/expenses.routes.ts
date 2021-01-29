fecf8d1dd5b12d8f3e654f4f01a45fb2aa1fdd89
caquijano <carlosquijano98@gmail.com>
2021-01-29 10:35:28 -0500

    upss

--- src/routes/expenses/expenses.routes.ts
+++ src/routes/expenses/expenses.routes.ts
@@ -3,14 +3,14 @@ import * as expenseController from './expenses.controller'
 
 const router = Router();
 
-router.get('/items', expenseController.getExpenses);
+router.get('/expenses', expenseController.getExpenses);
 
-router.get('/items/:id', expenseController.getExpense);
+router.get('/expenses/:id', expenseController.getExpense);
 
-router.post('/items', expenseController.createExpense);
+router.post('/expenses', expenseController.createExpense);
 
-router.delete('/items/:id', expenseController.deleteExpenses);
+router.delete('/expenses/:id', expenseController.deleteExpenses);
 
-router.put('/items/:id', expenseController.updateExpense);
+router.put('/expenses/:id', expenseController.updateExpense);
 
 export default router
