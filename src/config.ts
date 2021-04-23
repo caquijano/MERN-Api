94f877f98c71c60c54bd42d757dfdb74d93aabf2
caquijano <carlosquijano98@gmail.com>
2021-04-23 16:51:32 -0500

    koko

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,5 @@ export default{
     MONGO_DATABASE: 'ferreteria1',
     MONGO_USER: 'admin',
     MONGO_PASSWORD: 'admin',
-    MONGO_HOST: 'localhost'
+    MONGO_HOST: 'admin:admin@ikhodi.kmdva.mongodb.net'
 }
